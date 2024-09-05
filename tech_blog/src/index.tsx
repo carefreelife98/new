85c4eb557207ab2ae957409255e68728f1470127
CarefreeLife <csm4903@naver.com>
2024-09-06 01:59:43 +0900

    Github Actions 추가

--- tech_blog/src/index.tsx
+++ tech_blog/src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      {/* PUBLIC_URL 은 package.json의 homepage URL값으로 설정됨 */}
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+          <App />
+      </BrowserRouter>
   </React.StrictMode>
 );
