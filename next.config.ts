dbfd59d5ec13f859cdf23f153ebe76a08c0d15ed
채승민 <carefreelife98@flow.team>
2025-02-19 02:38:04 +0900

    refactor: 동적 라우팅 불가

--- next.config.ts
+++ next.config.ts
@@ -6,9 +6,10 @@ const nextConfig: NextConfig = {
     images: {
         unoptimized: true, // GitHub Pages 배포를 위해 필요
     },
+    trailingSlash: true, // 정적 HTML 파일이 디렉토리 구조로 배포되도록 설정
     // GitHub Pages에서 제공되는 서브 디렉토리 경로를 설정
     basePath: '/new',
-    assetPrefix: '/new/',
+    assetPrefix: '/new',
     env: {
         NEXT_PUBLIC_IMAGE_URL: process.env.NEXT_PUBLIC_IMAGE_URL || '/assets/images',
     },
